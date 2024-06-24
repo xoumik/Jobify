b1a92898433ef98c59c366a5407e7f880f6238f7
xoumik <shoumik21_ug@ece.nits.ac.in>
2024-06-24 18:48:03 +0530

    fix front-end

--- server.js
+++ server.js
@@ -32,24 +32,16 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
-app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.static(path.resolve(__dirname, './client/dist')));
 app.use(cookieParser());
 app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.send('hi');
-});
-
-app.get('/api/v1/test', (req, res) => {
-    res.json({ msg: 'test route' });
-});
 
 app.use('/api/v1/jobs', authenticateUser, jobRouter);
 app.use('/api/v1/users', authenticateUser, userRouter);
 app.use('/api/v1/auth', authRouter);
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './public', 'index.html'));
+    res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
 });
 
 app.use('*', (req, res) => {
